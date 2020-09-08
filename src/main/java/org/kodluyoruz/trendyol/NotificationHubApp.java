1aad090b6fea82434d5d2d88518479137e3b652a
MustafaCEVIK <cevikksoft@gmail.com>
2020-09-08 03:05:13 +0300

    Fix compiler error.

---
+++ src/main/java/org/kodluyoruz/trendyol/NotificationHubApp.java
@@ -0,0 +1,10 @@
+package org.kodluyoruz.trendyol;
+
+import org.kodluyoruz.trendyol.model.Company;
+
+public class NotificationHubApp {
+    public static void main(String[] args) {
+        Company company1 = new Company("Comp1");
+        Company company2 = new Company("Comp2");
+    }
+}
